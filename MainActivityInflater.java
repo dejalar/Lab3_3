2acb1e348eb3260ddb7983c2b672ee370a8e6003
dejalar <56512549+dejalar@users.noreply.github.com>
2021-05-31 04:54:38 +0300

    Update MainActivityInflater.java

--- MainActivityInflater.java
+++ MainActivityInflater.java
@@ -4,7 +4,7 @@ import android.annotation.SuppressLint;
 import android.os.Build;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
@@ -23,55 +23,29 @@ public class MainActivityInflater {
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public static void inflate(AppCompatActivity activity) {
 
-        EditText edittext_input_a = activity.findViewById(R.id.edittext_input_a);
-        EditText edittext_input_b = activity.findViewById(R.id.edittext_input_b);
-        EditText edittext_input_c = activity.findViewById(R.id.edittext_input_c);
-        EditText edittext_input_d = activity.findViewById(R.id.edittext_input_d);
 
-        EditText edittext_input_y = activity.findViewById(R.id.edittext_input_y);
 
         Button button_count = activity.findViewById(R.id.button_count);
 
-        Button button_count_with_different_fractions =
-                activity.findViewById(R.id.button_count_with_different_fractions);
+
 
         TextView textview_output_result = activity.findViewById(R.id.textview_output_result);
 
+
+
         @SuppressLint("SetTextI18n") View.OnClickListener onButtonCountClick = v -> {
+            int counter = 1;
+            long t= System.currentTimeMillis();
+            long end = t+3000;
+            while (System.currentTimeMillis() < end){
+                int o = 0; // Начальное значение диапазона - "от"
+                int p = 10; // Конечное значение диапазона - "до"
+                long a = o + (int) (Math.random() * p);
+                long b = o + (int) (Math.random() * p);
+                long c = o + (int) (Math.random() * p);
+                long d = o + (int) (Math.random() * p);
 
-            String string_a = String.valueOf(edittext_input_a.getText());
-            String string_b = String.valueOf(edittext_input_b.getText());
-            String string_c = String.valueOf(edittext_input_c.getText());
-            String string_d = String.valueOf(edittext_input_d.getText());
-
-            String string_y = String.valueOf(edittext_input_y.getText());
-
-            if (
-                    string_a.trim().equals("") || string_b.trim().equals("") ||
-                            string_c.trim().equals("") || string_d.trim().equals("") ||
-                            string_y.trim().equals("")
-            ) {
-
-                textview_output_result.setTextColor(activity.getResources().getColor(R.color.red));
-                textview_output_result.setText("Не введені дані");
-
-            } else if (
-                    string_a.trim().equals("-") || string_b.trim().equals("-") ||
-                            string_c.trim().equals("-") || string_d.trim().equals("-") ||
-                            string_y.trim().equals("-")
-            ) {
-
-                textview_output_result.setTextColor(activity.getResources().getColor(R.color.red));
-                textview_output_result.setText("Неправильно введені дані");
-
-            } else {
-
-                long a = Long.parseLong(string_a);
-                long b = Long.parseLong(string_b);
-                long c = Long.parseLong(string_c);
-                long d = Long.parseLong(string_d);
-
-                long y = Long.parseLong(string_y);
+                long y = o + (int) (Math.random() * p);
 
                 EquationSolver solver = new EquationSolver();
 
@@ -79,41 +53,49 @@ public class MainActivityInflater {
                 solver.setY(y);
 
                 try {
-
                     solver.solve();
+                } catch (ItersExceededException e) {
+                    e.printStackTrace();
+                }
 
-                    long[] roots = solver.getRoots();
+                long[] roots = solver.getRoots();
 
-                    Permissions permissions = new Permissions(roots);
+                Permissions permissions = new Permissions(roots);
 
-                    long[][] perms = permissions.getPerms();
+                long[][] perms = permissions.getPerms();
 
-                    for (long[] perm : perms) {
+                for (long[] perm : perms) {
 
-                        if (a * perm[0] + b * perm[1] + c * perm[2] + d * perm[3] == y)
-                            roots = perm;
-
-                    }
-
-                    StringBuilder out = new StringBuilder();
-
-                    for (int i = 0; i < roots.length; i++) {
-                        out.append("X").append(i + 1).append(" = ").append(roots[i]).append("\n");
-                    }
-
-                    textview_output_result.setTextColor(activity.getResources().getColor(R.color.green));
-                    textview_output_result.setText(out);
-
-
-                } catch (ItersExceededException exception) {
-
-                    textview_output_result.setTextColor(activity.getResources().getColor(R.color.red));
-                    textview_output_result.setText("Перевищена максимальна кількість ітерацій (1000000)");
+                    if (a * perm[0] + b * perm[1] + c * perm[2] + d * perm[3] == y)
+                        roots = perm;
 
                 }
 
+                StringBuilder out = new StringBuilder();
+
+                for (int i = 0; i < roots.length; i++) {
+                    out.append("X").append(i + 1).append(" = ").append(roots[i]).append("\n");
+                }
+                counter = counter + 1;
+
+
+
+
+
+
+
             }
+            textview_output_result.setTextColor(activity.getResources().getColor(R.color.red));
+            textview_output_result.setText("розв'язано прикладів - "+counter);
+
 
         };
 
-        
+
+
+        button_count.setOnClickListener(onButtonCountClick);
+
+
+    }
+
+}
